225893112b83f8cdf750ae5dc0d4b94cbe78f9d6
Angus Mark <ngsmrk@gmail.com>
2017-07-02 16:58:58 +0100

    Exercise 3 complete

--- src/main/java/org/adoptopenjdk/lambda/tutorial/exercise3/Books.java
+++ src/main/java/org/adoptopenjdk/lambda/tutorial/exercise3/Books.java
@@ -25,47 +25,48 @@ package org.adoptopenjdk.lambda.tutorial.exercise3;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
-
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
  * Domain object representing a collection of books
  */
 public class Books {
-    
+
     /**
      * Apply a mapping of Books to titles (Strings)
-     * 
+     *
      * @param books - books to transform
      * @return list of book titles
      */
     public static List<String> titlesOf(List<Book> books) {
-        // [your code here]
-        
-        return Collections.emptyList();
+        final Function<Book, String> function = Book::getTitle;
+        return applyMapFunction(books, function);
     }
 
     /**
      * Apply a mapping of Books to their author's full names
-     *  
+     *
      * @param books - books to transform
      * @return list of author full names
      */
     public static List<String> namesOfAuthorsOf(List<Book> books) {
-        // [your code here]
-        
-        return Collections.emptyList();
+        final Function<Book, String> function = book -> book.getAuthor().fullName();
+        return applyMapFunction(books, function);
+    }
+
+    private static List<String> applyMapFunction(List<Book> books, Function<Book, String> function) {
+        return Collections.unmodifiableList(books.stream().map(function).collect(Collectors.toList()));
     }
 
     /**
      * Apply a mapping of Books to a unique set of their publishers
-     * 
+     *
      * @param books - books to transform
      * @return set of publishers
      */
     public static Set<Publisher> publishersRepresentedBy(List<Book> books) {
-        // [your code here]
-
-        return Collections.emptySet();
+        final Function<Book, Publisher> function = book -> book.getPublisher();
+        return Collections.unmodifiableSet(books.stream().map(function).collect(Collectors.toSet()));
     }
 }
