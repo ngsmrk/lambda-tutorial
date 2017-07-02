5782d9182cef1040fa82f53fd3d3b33a1519a7ec
Angus Mark <ngsmrk@gmail.com>
2017-07-02 16:49:52 +0100

    Exercise 2 complete

--- src/main/java/org/adoptopenjdk/lambda/tutorial/exercise2/ElectoralDistrict.java
+++ src/main/java/org/adoptopenjdk/lambda/tutorial/exercise2/ElectoralDistrict.java
@@ -25,6 +25,7 @@ package org.adoptopenjdk.lambda.tutorial.exercise2;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Some (inaccurate) London electrical districts
@@ -54,9 +55,9 @@ public enum ElectoralDistrict {
      * @return filtered set of registered voters in a district
      */
     public static Set<RegisteredVoter> votersIn(ElectoralDistrict district, Collection<RegisteredVoter> voters) {
-        // [your code here]
+        final Set<RegisteredVoter> set = voters.stream().filter(registeredVoter -> registeredVoter.getElectorId().startsWith(district.getPrefix())).collect(Collectors.toSet());
 
-        return Collections.emptySet();
+        return Collections.unmodifiableSet(set);
     }
 
     /**
@@ -66,9 +67,7 @@ public enum ElectoralDistrict {
      * @return filtered set of unspoiled ballots
      */
     public static Set<Ballot> unspoiledBallots(Set<Ballot> ballots) {
-        // [your code here]
-
-        return Collections.emptySet();
+        return ballots.stream().filter(ballot -> !ballot.isSpoiled()).collect(Collectors.toSet());
     }
 
     public String getPrefix() {
